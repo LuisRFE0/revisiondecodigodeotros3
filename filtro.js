36b3d69308a3b9efb476d9935cf5b304929f00aa
Luis flores <luis.r.f.e@hotmail.com>
2023-12-08 10:28:31 -0600

    Corregi el programa

--- filtro.js
+++ filtro.js
@@ -1,63 +1,77 @@
+//Organice las variables y las puse al principio
+const li = document.querySelector("#lista-de-productos");
+const inputBuscar = document.querySelector('#input-buscar');
+const botonDeFiltro = document.querySelector("#btn-filtrar");
+botonDeFiltro.addEventListener('click', mostrarProductos);
 // Tenemos un li de productos
-
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+  { nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg" },
+  { nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg" },
+  { nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg" },
+  { nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg" },
+  { nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg" }
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+// Comente todo esto por que era duplicado*****************************************************
 
-  li.appendChild(d)
-}
+// for (let i = 0; i < productos.length; i++) {
+//   const div = document.createElement("div")
+//   div.classList.add("producto")
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
+//   const ti = document.createElement("p")
+//   ti.classList.add("titulo")
+//   ti.textContent = productos[i].nombre
 
-botonDeFiltro.onclick = function() {
+//   const imagen = document.createElement("img");
+//   imagen.setAttribute('src', productos[i].img);
+
+//   div.appendChild(ti)
+//   div.appendChild(imagen)
+
+//   li.appendChild(div)
+// }
+
+// displayProductos(productos)
+
+
+
+
+
+//Cambie el onclik por adevent listener y mejore la legibilidad
+function mostrarProductos() {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
 
-  const texto = $i.value;
-  console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
+  const texto = inputBuscar.value;
+  const productosFiltrados = filtrado(productos, texto);
+
 
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
-  
-    var ti = document.createElement("p")
+    const div = document.createElement("div")
+    div.classList.add("producto")
+
+    const ti = document.createElement("p")
     ti.classList.add("titulo")
     ti.textContent = productosFiltrados[i].nombre
-    
-    var imagen = document.createElement("img");
+
+    const imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
-  
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
+
+    const tipo = document.createElement("p");
+    tipo.textContent = productosFiltrados[i].tipo;
+
+    div.appendChild(ti)
+    div.appendChild(imagen)
+    div.appendChild(tipo)
+
+    li.appendChild(div);
   }
 }
 
-const filtrado = (productos = [], texto) => {
+const filtrado = (productos, texto) => {
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
 }  
